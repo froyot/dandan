783bdacbc235ede6f13ed89496661ee565f8fe63
Allon <xianlong.wang@dev-engine.com>
2015-11-27 15:53:36 +0800

    edit search view error

--- fronted/views/site/search.php
+++ fronted/views/site/search.php
@@ -1,22 +1,21 @@
 <?php
 
-use yii\helpers\Html;
 use yii\widgets\ListView;
 
 /* @var $this yii\web\View */
 /* @var $searchModel app\models\form\Content */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = \Yii::t('app','search {_keywords}:', [
+$this->title = \Yii::t('app', 'search {_keywords}:', [
     '_keywords' => Yii::$app->request->get('_keywords'),
 ]);
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="content-index">
-    <?= ListView::widget([
-        'dataProvider' => $dataProvider,
-        'itemView'=>'@app/views/content/listItemSearch',
-        'layout' => "{items}\n{pager}"
-    ]); ?>
+    <?=ListView::widget([
+'dataProvider' => $dataProvider,
+'itemView' => '/content/listItemSearch',
+'layout' => "{items}\n{pager}",
+]);?>
 
 </div>
